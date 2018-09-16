27a4dd0c2eca73bd56ff3700c0850ef19b591566
UCyborg <nejc.staric.23@gmail.com>
2018-09-16 05:47:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Half-Life 1.1.1.0 MP3 Fix
+
+Small ASI plugin that fixes the issue with not playing correct MP3 files when playing expansion packs/mods with the old pre-Steam version of Half-Life.
+For example, you would always hear the Half-Life soundtrack even if playing Half-Life: Opposing Force.
+
+Ability to play MP3 files as a substitute for playing soundtrack from CD first appeared with patch 1.1.0.9. It's an undocumented feature that only
+became official with Steam version of Half-Life. Refer to the following list to see how to name MP3 files after ripping them from Half-Life CD:
+
+1. <data track, ignore it>
+2. Half-Life01.mp3
+3. Prospero01.mp3
+4. Half-Life12.mp3
+5. Half-Life07.mp3
+6. Half-Life10.mp3
+7. Suspense01.mp3
+8. Suspense03.mp3
+9. Half-Life09.mp3
+10. Half-Life02.mp3
+11. Half-Life13.mp3
+12. Half-Life04.mp3
+13. Half-Life15.mp3
+14. Half-Life14.mp3
+15. Half-Life16.mp3
+16. Suspense02.mp3
+17. Half-Life03.mp3
+18. Half-Life08.mp3
+19. Prospero02.mp3
+20. Half-Life05.mp3
+21. Prospero04.mp3
+22. Half-Life11.mp3
+23. Half-Life06.mp3
+24. Prospero03.mp3
+25. Half-Life17.mp3
+26. Prospero05.mp3
+27. Suspense05.mp3
+28. Suspense07.mp3
+
+The files go into following folders. The above list can be used when ripping CDs of the expansion packs, the only difference is they have less tracks.
+
+### Half-Life
+
+\Half-Life\valve\media
+
+### Half-Life: Opposing Force
+
+\Half-Life\gearbox\media
+
+### Half-Life: Blue Shift
+
+\Half-Life\bshift_unlocked\media (use [Blue Shift: Unlocked v1.1](https://www.moddb.com/games/half-life-blue-shift/downloads/blue-shift-unlocked-v112) to convert your BS installation to HL mod)
---
+++ dllmain.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#pragma comment(lib, "Version")
+
+int (__stdcall *O_AIL_open_stream)(void *, char *, int);
+int __stdcall H_AIL_open_stream(void *ptr, char *stream, int arg3)
+{
+	char szPath[MAX_PATH];
+	char *temp;
+	int ret;
+	char *basePath = (char *)0x3CFA788;
+
+	temp = strrchr(basePath, '/');
+
+	sprintf(szPath, "%s%s", temp ? temp + 1 : basePath, stream + 5);
+	if (ret = O_AIL_open_stream(ptr, szPath, arg3)) return ret;
+
+	return O_AIL_open_stream(ptr, stream, arg3);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	DWORD_PTR dwPatchBase;
+	DWORD dwOldProtect;
+
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		char szPath[MAX_PATH];
+		DWORD dwSize;
+		DWORD dwHandle;
+		void *pVersionInfo;
+		char *pStringFileInfoValue;
+		UINT fileInfoLen;
+
+		DisableThreadLibraryCalls(hinstDLL);
+
+		GetModuleFileName(NULL, szPath, sizeof(szPath));
+
+		if (!(dwSize = GetFileVersionInfoSize(szPath, &dwHandle)))
+		{
+			goto fail;
+		}
+
+		if (!(pVersionInfo = malloc(dwSize)))
+		{
+			goto fail;
+		}
+
+		if (!GetFileVersionInfo(szPath, dwHandle, dwSize, pVersionInfo)
+			|| !VerQueryValue(pVersionInfo, "\\StringFileInfo\\040904b0\\FileDescription", (LPVOID *)&pStringFileInfoValue, &fileInfoLen)
+			|| strcmp(pStringFileInfoValue, "Half-Life Launcher")
+			|| !VerQueryValue(pVersionInfo, "\\StringFileInfo\\040904b0\\FileVersion", (LPVOID *)&pStringFileInfoValue, &fileInfoLen)
+			|| strcmp(pStringFileInfoValue, "1, 1, 1, 0"))
+		{
+			free(pVersionInfo);
+fail:		return FALSE;
+		}
+
+		free(pVersionInfo);
+
+		dwPatchBase = 0x4AD7E8;
+		VirtualProtect((LPVOID)dwPatchBase, sizeof(DWORD_PTR), PAGE_READWRITE, &dwOldProtect);
+
+		(DWORD_PTR)O_AIL_open_stream = *(DWORD_PTR *)dwPatchBase;
+		*(DWORD_PTR *)dwPatchBase = (DWORD_PTR)H_AIL_open_stream;
+
+		VirtualProtect((LPVOID)dwPatchBase, sizeof(DWORD_PTR), dwOldProtect, &dwOldProtect);
+	}
+	else if (fdwReason == DLL_PROCESS_DETACH)
+	{
+		if (O_AIL_open_stream)
+		{
+			dwPatchBase = 0x4AD7E8;
+			VirtualProtect((LPVOID)dwPatchBase, sizeof(DWORD_PTR), PAGE_READWRITE, &dwOldProtect);
+
+			*(DWORD_PTR *)dwPatchBase = (DWORD_PTR)O_AIL_open_stream;
+
+			VirtualProtect((LPVOID)dwPatchBase, sizeof(DWORD_PTR), dwOldProtect, &dwOldProtect);
+		}
+	}
+	return TRUE;
+}
